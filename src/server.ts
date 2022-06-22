2505ba8687484ef6051f761e681facce08f729d2
Helder Correia <helder.patrick25@gmail.com>
2022-06-22 08:34:11 +0100

    Setup debugger

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (request, response) => {
-  return response.json({ message: ' Hello World ' });
+  return response.json({ message: 'Hello World' });
 });
 app.listen(3333, () => console.log('Server started'));
