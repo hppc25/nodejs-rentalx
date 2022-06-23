65d14d3104fd81f4e56b6bec87d0344b7a4138a1
Helder Correia <helder.patrick25@gmail.com>
2022-06-23 08:17:33 +0100

    implements useCase on createCategory

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,19 +1,18 @@
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
-} from '../repositories/ICategoriesRepository';
+} from '../../repositories/ICategoriesRepository';
 
 /**
  * [x] - Set return type
  * [x] - Change return error
  * [x] - Access the repository
  */
-class CreateCategoryService {
+class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
   execute({ name, description }: ICreateCategoryDTO): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
-
     if (categoryAlreadyExists) {
       throw new Error('Category already exists!');
     }
@@ -22,4 +21,4 @@ class CreateCategoryService {
   }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
