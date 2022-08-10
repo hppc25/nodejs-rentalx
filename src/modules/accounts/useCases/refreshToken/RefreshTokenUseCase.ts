0877e6c5329954685b68724d618f659562c1740f
Helder Correia <helder.patrick25@gmail.com>
2022-08-10 07:28:16 +0100

    implements refresh token controller,  use case and route

---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,58 @@
+import { sign, verify } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import auth from '@config/auth';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { AppError } from '@shared/errors/AppError';
+
+interface IPayload {
+  email: string;
+  sub: string;
+}
+@injectable()
+class RefreshTokenUseCase {
+  constructor(
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject('DateProvider')
+    private dateProvider: IDateProvider,
+  ) {}
+
+  async execute(token: string): Promise<string> {
+    const { sub, email } = verify(token, auth.secret_refresh_token) as IPayload;
+
+    const user_id = sub;
+
+    const userToken =
+      await this.usersTokensRepository.findByUserIdAndRefreshToken(
+        user_id,
+        token,
+      );
+
+    if (!userToken) {
+      throw new AppError('Refresh Token does not exists!');
+    }
+
+    await this.usersTokensRepository.deleteById(userToken.id);
+
+    const refresh_token = sign({ email }, auth.secret_refresh_token, {
+      subject: sub,
+      expiresIn: auth.expires_in_refresh_token,
+    });
+
+    const refresh_token_expires_date = this.dateProvider.addDays(
+      auth.expires_refresh_token_days,
+    );
+
+    await this.usersTokensRepository.create({
+      user_id: sub,
+      refresh_token,
+      expires_date: refresh_token_expires_date,
+    });
+
+    return refresh_token;
+  }
+}
+
+export { RefreshTokenUseCase };
